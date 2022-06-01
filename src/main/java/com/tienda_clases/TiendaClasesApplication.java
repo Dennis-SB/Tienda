fc13e31f88f1d157b284085d9abe8c1de36ed190
Dennis Serrano <dserrano10115@ufide.ac.cr>
2022-05-31 18:27:35 -0600

    Primer Commit

---
+++ src/main/java/com/tienda_clases/TiendaClasesApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_clases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaClasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaClasesApplication.class, args);
+	}
+
+}
